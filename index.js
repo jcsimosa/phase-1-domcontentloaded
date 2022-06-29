4d2550ec3a0b019af37104ca8ca954127198ce9b
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-06-29 15:41:31 -0500

    commit tree b116485751e6950ac6b20666bb0a3342bd1a185c

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => { 
+    let changeN = document.getElementById('text')
+    changeN.textContent = 'This is really cool!' 
+    console.log('The DOM has loaded');
+})
+
